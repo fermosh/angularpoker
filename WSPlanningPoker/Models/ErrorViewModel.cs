7d034c3ead15ab7de96f6bfce7e3b0319d4dbe0b
Oscar Legorreta <Oscar_Legorreta@epam.com>
2018-01-17 17:02:42 -0600

    Initial scaffolding

---
+++ WSPlanningPoker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WSPlanningPoker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
